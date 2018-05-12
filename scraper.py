a5692e6ba3d3149e22d200d03491d4f05e217a81
Sam Sawan <ssawan@onshift.com>
2018-05-12 16:24:04 -0400

    better functional code

--- scraper.py
+++ scraper.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.common.exceptions import TimeoutException
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -13,12 +10,13 @@ parser.add_argument('password', help='the password for said email address')
 args = parser.parse_args()
 
 
-def login():
-    browser.find_element_by_id('emailAddress').send_keys(args.email)
-    browser.find_element_by_id('password').send_keys(args.password)
-    browser.find_element_by_class_name('btn-submit').send_keys(Keys.ENTER)
+def login(b):
+    b.find_element_by_id('emailAddress').send_keys(args.email)
+    b.find_element_by_id('password').send_keys(args.password)
+    b.find_element_by_class_name('btn-submit').send_keys(Keys.ENTER)
 
-if(__name__ == '__main__'):
+
+if __name__ == '__main__':
     browser = webdriver.Chrome(args.driver)
     browser.get('https://bootcampspot-v2.com/login')
-    login()
+    login(browser)
