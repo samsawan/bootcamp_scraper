66ee92185d3ae4f1cc9bf28c7bf0c4c42aa9055d
Sam Sawan <ssawan@onshift.com>
2018-05-12 23:58:19 -0400

    finding only the selected assignments

--- scraper.py
+++ scraper.py
@@ -3,11 +3,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import argparse
 import time
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument('driver', help='the absolute location to the Chrome driver')
 parser.add_argument('email', help='the email address you use to login to bcs')
 parser.add_argument('password', help='login password')
+parser.add_argument('assignment', help='the assignment number')
 
 args = parser.parse_args()
 
@@ -32,3 +34,13 @@ if __name__ == '__main__':
     browser.get('https://bootcampspot-v2.com/login')
     login(browser)
     gradebook(browser)
+    time.sleep(1)
+    assignment_table = browser.find_element_by_class_name('bcs-table')
+    assignment_entries = assignment_table.find_elements(By.XPATH, '//tbody/tr')
+    # todo assignment should be a class
+    assignment_title_regex = re.compile(f"{args.assignment}:")
+    valid_assignments = []
+    for assignment in assignment_entries:
+        assignment_title = assignment.find_element(By.XPATH, 'td[1]')
+        if assignment_title_regex.match(assignment_title.text):
+            valid_assignments.append(assignment)
